67f959be939c1dfcaa92d0935821e33a9e697c42
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-06 12:40:38 +0600

    Auto: 1 added, 1 modified - 2025-09-06

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import type { Idea } from '@/types';
 
-const fetchData = async (ideaId: string) => {
+const fetchData = async (ideaId: string): Promise<Idea> => {
   const res = await fetch(`/api/ideas/${ideaId}`);
 
   if (!res.ok) throw new Error('Failed to fetch data');
@@ -25,5 +26,13 @@ export const Route = createFileRoute('/ideas/$ideaId/')({
 function IdeaDetailsPage() {
   const { ideaId } = Route.useParams();
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
-  return <div>{idea.title}</div>;
+  return (
+    <div className='p-4'>
+      <Link to='/ideas' className='text-blue-500 underline block mb-4'>
+        Back To Ideas
+      </Link>
+      <h2 className='text-2xl font-bold'>{idea.title}</h2>
+      <p className='mt-2'>{idea.description}</p>
+    </div>
+  );
 }
