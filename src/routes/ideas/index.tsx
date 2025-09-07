aabbef7b3874500f1f1ca508928022fc8b6cac54
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-07 08:18:15 +0600

    Auto: 1 modified - 2025-09-07

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -29,5 +29,25 @@ export const Route = createFileRoute('/ideas/')({
 });
 
 function IdeasPage() {
-  return <div>Hello "/ideas/"!</div>;
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold mb-4'>Ideas</h1>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
+        {ideas.map((idea) => (
+          <li
+            key={idea.id}
+            className='border border-gray-300 p-4 rounded shadow bg-white flex flex-col justify-between'
+          >
+            <div>
+              <h2 className='text-lg font-semibold'>{idea.title}</h2>
+              <p className='text-gray-700 mt-2'>{idea.summary}</p>
+            </div>
+            <Link to=''>View Ideas</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
