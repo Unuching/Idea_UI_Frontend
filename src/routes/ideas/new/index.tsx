723bd13971518ff9697a31c5044aabc593b0edb0
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-08 20:56:42 +0600

    Auto: 3 modified - 2025-09-08

--- src/routes/ideas/new/index.tsx
+++ src/routes/ideas/new/index.tsx
@@ -1,9 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
+import type { Idea } from '@/types';
 
 export const Route = createFileRoute('/ideas/new/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/ideas/new/"!</div>
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState('');
+
+  return (
+    <div className='space-y-6 '>
+      <h1 className='text-3xl font-bold mb-6'>Create New Idea</h1>
+      <form className='space-y-4'>
+        <div>
+          <label
+            htmlFor='title'
+            className='block text-gray-700 font-medium mb-1'
+          >
+            Title
+          </label>
+          <input
+            id='title'
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='w-full'
+            placeholder='Enter New Idea'
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
