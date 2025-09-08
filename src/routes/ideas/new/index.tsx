aa3447a6fbf0ee33383daa9beefd95531d5859e9
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-08 21:22:07 +0600

    Auto: 1 modified - 2025-09-08

--- src/routes/ideas/new/index.tsx
+++ src/routes/ideas/new/index.tsx
@@ -29,9 +29,65 @@ function RouteComponent() {
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='w-full'
+            className='w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
             placeholder='Enter New Idea'
           />
+        </div>
+        <div>
+          <label
+            htmlFor='summary'
+            className='block text-gray-700 font-medium mb-1 '
+          >
+            Summary
+          </label>
+          <input
+            id='summary'
+            type='text'
+            value={summary}
+            onChange={(e) => setSummary(e.target.value)}
+            className='w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            placeholder='Enter Idea Summary'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='description'
+            className='block text-gray-700 font-medium mb-1 '
+          >
+            Description
+          </label>
+          <textarea
+            id='description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            rows={6}
+            className='w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            placeholder='Write the descrition of your idea...'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='tags'
+            className='block text-gray-700 font-medium mb-1 '
+          >
+            Tags
+          </label>
+          <input
+            id='tags'
+            type='text'
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+            className='w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            placeholder='Optional tags, comma separated'
+          />
+        </div>
+        <div className='mt-6'>
+          <button
+            type='submit'
+            className='block w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-md transition disabled:opacity-50 disabled:cursor-not-allowed'
+          >
+            Create Idea
+          </button>
         </div>
       </form>
     </div>
