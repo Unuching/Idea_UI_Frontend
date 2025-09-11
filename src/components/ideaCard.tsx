4d9520cce3e16b6141eaa03805de86ed7f128a9d
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-11 15:16:17 +0600

    Auto: 5 modified - 2025-09-11

--- src/components/ideaCard.tsx
+++ src/components/ideaCard.tsx
@@ -23,7 +23,7 @@ const IdeaCard = ({
       </div>
       <Link
         to='/ideas/$ideaId'
-        params={{ ideaId: idea.id.toString() }}
+        params={{ ideaId: idea._id.toString() }}
         className={linkClasses}
       >
         {button ? 'View Idea' : 'Read More'}
--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        
       },
     },
   },
