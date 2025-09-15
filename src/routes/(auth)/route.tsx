2191cb07a0777a9e2ab68e6b73c0c7ac6f89d215
Unuching Marma <unuching.marma2023@gmail.com>
2025-09-15 21:20:34 +0600

    Auto: 1 added, 1 modified - 2025-09-15

---
+++ src/routes/(auth)/route.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/(auth)')({
+  component: AuthLayout,
+});
+
+function AuthLayout() {
+  return <div>Hello "/(auth)/authRoute"!</div>;
+}
